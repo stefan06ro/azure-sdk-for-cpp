9b8ca50ef4aafb24bedcf6fefa0bfb9af1d8d5c8
Daniel Jurek <djurek@microsoft.com>
2021-02-25 22:29:12 -0800

    Validate publishing before parameter refactor

---
+++ eng/scripts/Update-VcpkgPort.ps1
@@ -0,0 +1,76 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $SourceDirectory,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $DestinationDirectory,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $VcpkgPortName,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $ChangelogLocation,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $PackageVersion,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $GitCommitParameters,
+
+    [switch] $PerformFinalCommit = $false 
+)
+
+# Load functions for operations on CHANGELOG.md
+."$PSScriptRoot/../common/scripts/ChangeLog-Operations.ps1"
+
+# Clean out the folder so that template files removed are not inadvertently 
+# re-added
+if (Test-Path $DestinationDirectory) {
+    Remove-Item -v -r $DestinationDirectory
+}
+
+New-Item -Type Directory $DestinationDirectory
+Copy-Item `
+    -Verbose `
+    "$SourceDirectory/*" `
+    $DestinationDirectory
+
+# Show artifacts copied into ports folder for PR
+Write-Host "Files in destination directory:" 
+Get-ChildItem -Recurse $DestinationDirectory | Out-String | Write-Host
+
+$commitMessageFile = New-TemporaryFile
+$chagelogEntry = Get-ChangeLogEntryAsString `
+    -ChangeLogLocation $ChangelogLocation `
+    -VersionString $PackageVersion
+"# $VcpkgPortName`n$chagelogEntry" | Set-Content $commitMessageFile
+
+Write-Host "Commit Message:"
+Write-host (Get-Content $commitMessageFile -Raw)
+
+Write-Host "git status"
+git status
+
+# Commit changes
+Write-Host "git add -A"
+git add -A
+Write-Host "git $GitCommitParameters commit --file $commitMessageFile"
+"git $GitCommitParameters commit --file $commitMessageFile" | Invoke-Expression -Verbose | Write-Host
+
+
+Write-Host "./bootstrap-vcpkg.bat"
+./bootstrap-vcpkg.bat
+
+Write-Host "./vcpkg.exe x-add-version $VcpkgPortName"
+./vcpkg.exe x-add-version $VcpkgPortName
+
+if ($PerformFinalCommit) { 
+    Write-Host "git $GitCommitParameters commit -m `"Update vcpkg version metadata for $VcpkgPortName`""
+    . "git $GitCommitParameters commit  -m `"Update vcpkg version metadata for $VcpkgPortName`""
+}
